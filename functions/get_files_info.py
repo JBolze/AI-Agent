acc40b1c7af5ab130b376ea5c5ab3e2615841026
JBolze <j.bolze94@gmail.com>
2025-08-10 21:54:16 -0300

    Started working on implementing the declaration for the LLM

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,5 @@
 import os
+import types
 
 
 def get_files_info(working_directory, directory="."):
@@ -35,3 +36,18 @@ def get_files_info(working_directory, directory="."):
         return "\n".join(result_lines)
     except Exception as e:
         return f'Error: An unexpected error occurred: {str(e)}'
+    
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
--- main.py
+++ main.py
@@ -8,11 +8,22 @@ from google.genai import types
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
+# System prompt
+system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+
+# Model name
+model_name = "gemini-2.0-flash-001"
+
 if len(sys.argv) < 2:
     print("Error: No prompt given.\nUsage: python main.py <your prompt here>")
     sys.exit(1)
 prompt = sys.argv[1]
 verbose = "--verbose" in sys.argv[2:]
+
+# List of past messages
+messages = [
+    types.Content(role="user", parts=[types.Part(text=prompt)]),
+]
 
 # Create client
 client = genai.Client(api_key=api_key)
@@ -20,7 +31,8 @@ client = genai.Client(api_key=api_key)
 # Generate content
 response = client.models.generate_content(
     model="gemini-2.0-flash-001",
-    contents=prompt
+    contents=messages,
+    config = types.GenerateContentConfig(system_instruction=system_prompt),
 )
 
 # Print the response
@@ -32,8 +44,4 @@ if verbose:
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-# List of past messages
-messages = [
-    types.Content(role="user", parts=[types.Part(text=prompt)]),
-]
 
